a87c3ec2e22e075cfa085446523a19f2bf86c5e8
Benavides Martinez <rbenavides@sigma-alimentos.com>
2020-01-17 11:38:51 -0600

    agregando datos y script

---
+++ mapa_choques_SP.R
@@ -0,0 +1,69 @@
+#Autor: Rodrigo Benavides
+#Fecha: Enero 17, 2020
+
+#Objetivo del script es poder visualizar en un mapa donde es la ubicación con mayor cantidad de choques en San Pedro.
+#Los datos son de Enero 2014 a Septiembre del 2016.
+library(tidyverse)
+library(leaflet)
+library(sf)
+
+#Importar datos y cambiar formato de nombres de las calles
+registro_accidentes <- read_csv("~/ACCIDENTES_CRUCEROS_SAN_PEDRO.csv")%>%
+  rename_all(iconv) %>%
+  mutate(calle1 = iconv(calle1),
+         calle2 = iconv(calle2),
+         fechaCorte = as.Date(fechaCorte, format = "%d/%m/%Y"))
+
+head(registro_accidentes)  
+
+#Quedarnos con los nombres de los cruces para despues pegar sus coordenadas
+nombres_cruces <- registro_accidentes %>%
+  distinct(calle1,calle2)%>%
+  arrange(calle1, calle2)
+
+coordenadas_cruces <- data.frame(
+  calle1 = nombres_cruces$calle1,
+  calle2 = nombres_cruces$calle2,
+  latitud = c(25.65716, 25.65381, 25.65945, 25.67473, 25.67048,
+              25.64012, 25.65275, 25.66262, 25.66788, 25.65179, 
+              25.65158, 25.65399, 25.65214, 25.65337, 25.65380, 
+              25.63820, 25.66150, 25.65828, 25.65032, 25.66906
+              ),
+  longitud = c(-100.37038, -100.35807, -100.36950, -100.40281, -100.40226,
+               -100.36191, -100.35833, -100.35458, -100.38026, -100.36599, 
+               -100.37154, -100.33980, -100.37795, -100.35280, -100.38121, 
+               -100.33793, -100.36877, -100.36533, -100.35865, -100.38486
+               )
+           )%>%
+  mutate(calle1 = as.character(calle1),
+         calle2 = as.character(calle2))
+
+#Obtener la cantidad total de choques por cruce.
+cantidad_por_cruce <- registro_accidentes %>%
+  group_by(calle1, calle2) %>%
+  summarise(cantidad_de_choques = sum(Total),
+            por_mes = round(cantidad_de_choques/6, digits = 1)) %>%
+  arrange(desc(cantidad_de_choques))%>%
+  ungroup()
+
+#Pegar el resumen estadístico con las coordenadas. Popup es para el despliegue en el mapa.
+reporte_cruces <- left_join(cantidad_por_cruce, coordenadas_cruces, by = c("calle1", "calle2"))%>%
+  mutate(popup = paste("<b>","Cantidad de Choques: ", cantidad_de_choques,"</b><br>",
+                       "Calle 1: ", calle1, "  <br>",
+                       "Calle 2: ", calle2, sep = '')
+         )
+ 
+#Cambiar dataframe a objeto sf para desplegar en el mapa
+reporte_cruces_sf <- st_as_sf(x = reporte_cruces, 
+                coords = c("longitud", "latitud"),
+                crs = "+proj=longlat +datum=WGS84")
+
+#Desplegar mapa
+leaflet(data = reporte_cruces_sf)%>%
+  addTiles() %>%
+  addProviderTiles(providers$OpenStreetMap) %>%
+  addCircleMarkers(popup = ~popup,
+                   radius = ~cantidad_de_choques**(1/2.5), 
+                   color = "red",
+                   stroke = 0, 
+                   fillOpacity = 1)
